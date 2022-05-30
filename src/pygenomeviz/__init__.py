b14ba0180724a1ca3430ee461e7a4165340c28fe
moshi <sy.41211@gmail.com>
2022-05-30 21:55:00 +0900

    Delete no required import in __all__

--- src/pygenomeviz/__init__.py
+++ src/pygenomeviz/__init__.py
@@ -1,18 +1,9 @@
-from pygenomeviz.feature import Feature
 from pygenomeviz.genbank import Genbank
 from pygenomeviz.genomeviz import GenomeViz
-from pygenomeviz.link import Link
-from pygenomeviz.track import FeatureTrack, LinkTrack, TickTrack, Track
 
 __version__ = "0.0.2"
 
 __all__ = [
-    "Feature",
     "GenomeViz",
-    "Link",
-    "Track",
-    "FeatureTrack",
-    "LinkTrack",
-    "TickTrack",
     "Genbank",
 ]

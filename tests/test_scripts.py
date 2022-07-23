0e53481117f717af8d429590aa499a8d702df5a7
moshi <sy.41211@gmail.com>
2022-07-23 18:52:43 +0900

    Add CLI workflow test case

---
+++ tests/test_scripts.py
@@ -0,0 +1,69 @@
+import subprocess as sp
+from pathlib import Path
+
+import pytest
+from pygenomeviz.scripts.mmseqs import MMseqs
+from pygenomeviz.scripts.mummer import MUMmer
+from pygenomeviz.scripts.pmauve import ProgressiveMauve
+from pygenomeviz.utils import load_dataset
+
+
+def test_download_dataset_cli(tmp_path: Path):
+    """Test download dataset CLI (pgv-download-dataset)"""
+    dataset_name = "escherichia_phage"
+    cmd = f"pgv-download-dataset -n {dataset_name} -o {tmp_path}"
+    result = sp.run(cmd, shell=True)
+
+    assert result.returncode == 0
+    assert len(list(tmp_path.glob("*.gbk"))) == 4
+
+
+@pytest.mark.skipif(
+    condition=not MUMmer.check_installation(),
+    reason="MUMmer is not installed in this environment.",
+)
+def test_mummer_cli(tmp_path: Path):
+    """Test MUMmer CLI (pgv-mummer)"""
+    gbk_files, _ = load_dataset("escherichia_phage")
+    gbk_files = [str(f) for f in gbk_files]
+    cmd = f"pgv-mummer --gbk_resources {' '.join(gbk_files)} -o {tmp_path} --curve"
+    result = sp.run(cmd, shell=True)
+
+    assert result.returncode == 0
+
+    result_fig_file = tmp_path / "result.png"
+    assert result_fig_file.exists()
+
+
+@pytest.mark.skipif(
+    condition=not MMseqs.check_installation(),
+    reason="MMseqs is not installed in this environment.",
+)
+def test_mmseqs_cli(tmp_path: Path):
+    """Test MMseqs CLI (pgv-mmseqs)"""
+    gbk_files, _ = load_dataset("escherichia_phage")
+    gbk_files = [str(f) for f in gbk_files]
+    cmd = f"pgv-mmseqs --gbk_resources {' '.join(gbk_files)} -o {tmp_path} --curve"
+    result = sp.run(cmd, shell=True)
+
+    assert result.returncode == 0
+
+    result_fig_file = tmp_path / "result.png"
+    assert result_fig_file.exists()
+
+
+@pytest.mark.skipif(
+    condition=not ProgressiveMauve.check_installation(),
+    reason="progressiveMauve is not installed in this environment.",
+)
+def test_pmauve_cli(tmp_path: Path):
+    """Test progressiveMauve CLI (pgv-pmauve)"""
+    gbk_files, _ = load_dataset("escherichia_phage")
+    gbk_files = [str(f) for f in gbk_files]
+    cmd = f"pgv-pmauve --seq_files {' '.join(gbk_files)} -o {tmp_path} --curve"
+    result = sp.run(cmd, shell=True)
+
+    assert result.returncode == 0
+
+    result_fig_file = tmp_path / "result.png"
+    assert result_fig_file.exists()

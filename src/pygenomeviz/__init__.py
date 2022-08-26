b36ec576e98f48dc945f50affb91d8fa3734f5f3
moshi <sy.41211@gmail.com>
2022-08-27 01:56:03 +0900

    Change v0.2.1 to v0.2.2

--- src/pygenomeviz/__init__.py
+++ src/pygenomeviz/__init__.py
@@ -4,7 +4,7 @@ from pygenomeviz.genbank import Genbank
 from pygenomeviz.genomeviz import GenomeViz
 from pygenomeviz.utils import load_dataset
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "GenomeViz",

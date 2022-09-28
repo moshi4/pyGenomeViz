39701d6a76f4cbd373d58b78322e3971c354aa17
moshi <sy.41211@gmail.com>
2022-09-28 21:45:44 +0900

    Change matplotlib svg.fonttype = "none"

--- src/pygenomeviz/__init__.py
+++ src/pygenomeviz/__init__.py
@@ -27,5 +27,7 @@ mpl_rc_params = {
     # Savefig
     "savefig.bbox": "tight",  # Default: None
     "savefig.pad_inches": 0.5,  # Default: 0.1
+    # SVG
+    "svg.fonttype": "none",
 }
 mpl.rcParams.update(mpl_rc_params)

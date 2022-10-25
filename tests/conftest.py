59cc3684bbe2057b3ff9d0039784dbfbf9f31f36
moshi <sy.41211@gmail.com>
2022-10-25 19:30:28 +0900

    Add GFF parse test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,32 +2,71 @@ from pathlib import Path
 
 import pytest
 
-
-@pytest.fixture
-def testdata_dir() -> Path:
-    """Testdata directory"""
-    return Path(__file__).parent / "testdata"
+###########################################################
+# Genbank test data
+###########################################################
 
 
 @pytest.fixture
-def gbk_file(testdata_dir: Path) -> Path:
+def gbk_testdata_dir() -> Path:
+    """Genbank testdata directory"""
+    return Path(__file__).parent / "testdata" / "genbank"
+
+
+@pytest.fixture
+def gbk_file(gbk_testdata_dir: Path) -> Path:
     """Genbank file"""
-    return testdata_dir / "test.gbff"
+    return gbk_testdata_dir / "test.gbff"
 
 
 @pytest.fixture
-def gbk_bzfile(testdata_dir: Path) -> Path:
+def gbk_bzfile(gbk_testdata_dir: Path) -> Path:
     """Genbank file (bz2 compressed)"""
-    return testdata_dir / "test.gbff.bz2"
+    return gbk_testdata_dir / "test.gbff.bz2"
 
 
 @pytest.fixture
-def gbk_gzfile(testdata_dir: Path) -> Path:
+def gbk_gzfile(gbk_testdata_dir: Path) -> Path:
     """Genbank file (gz compressed)"""
-    return testdata_dir / "test.gbff.gz"
+    return gbk_testdata_dir / "test.gbff.gz"
 
 
 @pytest.fixture
-def gbk_zipfile(testdata_dir: Path) -> Path:
+def gbk_zipfile(gbk_testdata_dir: Path) -> Path:
     """Genbank file (zip compressed)"""
-    return testdata_dir / "test.zip"
+    return gbk_testdata_dir / "test.zip"
+
+
+###########################################################
+# GFF test data
+###########################################################
+
+
+@pytest.fixture
+def gff_testdata_dir() -> Path:
+    """GFF testdata directory"""
+    return Path(__file__).parent / "testdata" / "gff"
+
+
+@pytest.fixture
+def gff_file(gff_testdata_dir: Path) -> Path:
+    """GFF file"""
+    return gff_testdata_dir / "test.gff"
+
+
+@pytest.fixture
+def gff_bzfile(gff_testdata_dir: Path) -> Path:
+    """GFF file (bz2 compressed)"""
+    return gff_testdata_dir / "test.gff.bz2"
+
+
+@pytest.fixture
+def gff_gzfile(gff_testdata_dir: Path) -> Path:
+    """GFF file (gz compressed)"""
+    return gff_testdata_dir / "test.gff.gz"
+
+
+@pytest.fixture
+def gff_zipfile(gff_testdata_dir: Path) -> Path:
+    """GFF file (zip compressed)"""
+    return gff_testdata_dir / "test.zip"
---
+++ tests/parser/test_gff.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+
+import pytest
+from pygenomeviz import Gff
+
+
+def test_gff_load_default(gff_file: Path):
+    """Test gff load default"""
+    gff = Gff(gff_file)
+    assert gff.name == "test"
+    assert gff.min_range == 0
+    assert gff.max_range == 66854
+    assert gff.range_size == 66854
+
+
+def test_gff_load_user_params(gff_file: Path):
+    """Test gff load user params"""
+    name, seqid, min_range, max_range = "user", "JX128258.1", 10000, 30000
+    gff = Gff(
+        gff_file,
+        name=name,
+        target_seqid=seqid,
+        min_range=min_range,
+        max_range=max_range,
+    )
+    assert gff.name == name
+    assert gff.min_range == min_range
+    assert gff.max_range == max_range
+    assert gff.range_size == max_range - min_range
+    assert seqid == gff.seqid_list[0]
+
+
+def test_gff_load_seqid_exist_error(gff_file: Path):
+    """Test gff load seqid exist error"""
+    with pytest.raises(ValueError):
+        Gff(gff_file, target_seqid="no_exists")
+
+
+def test_gff_load_range_error(gff_file: Path):
+    """Test gff load range error"""
+    # Case1. min_range < 0
+    with pytest.raises(ValueError):
+        Gff(gff_file, min_range=-10000, max_range=40000)
+    # Case2. min_range > max_range
+    with pytest.raises(ValueError):
+        Gff(gff_file, min_range=40000, max_range=10000)
+
+
+def test_extract_features(gff_file: Path):
+    """Test extract_features"""
+    gff = Gff(gff_file)
+    cds_features = gff.extract_features()
+    assert len(cds_features) == 94
+
+    no_features = gff.extract_features("no_data")
+    assert len(no_features) == 0
+
+
+def test_extract_features_restrict_range(gff_file: Path):
+    """Test extract_features with restrict range"""
+    gff = Gff(gff_file, min_range=0, max_range=10000)
+    features = gff.extract_features()
+    assert len(features) == 22
+
+
+def test_parse_bzfile(gff_bzfile: Path):
+    """Test parse GFF file (bz2 compressed)"""
+    gff = Gff(gff_bzfile)
+    assert gff.name == "test"
+
+
+def test_parse_gzfile(gff_gzfile: Path):
+    """Test parse GFF file (gz compressed)"""
+    gff = Gff(gff_gzfile)
+    assert gff.name == "test"
+
+
+def test_parse_zipfile(gff_zipfile: Path):
+    """Test parse GFF file (zip compressed)"""
+    gff = Gff(gff_zipfile)
+    assert gff.name == "test"
